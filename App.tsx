073a9ddd07ad6561a72480b16af22bc4f63cdd5b
brianwachira <brianwachira7@gmail.com>
2023-02-18 23:27:47 +0300

    [enhancement] : add custom theme

--- App.tsx
+++ App.tsx
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, Box, extendTheme } from "native-base";
+import useCachedResources from './src/hooks/useCachedResources';
 
 export default function App() {
+
+  const theme = extendTheme({
+    colors: {
+      gelcargoWhite: '#FFFFFF',
+      gelcargoBlack: '#27282E',
+      gelcargoGrey:  '#929395',
+      gelcargoGreyLight:  '#929395',
+      gelcargoBlue:  '#3A69BB',
+      gelcargoYellow:  '#F4A124',
+    },
+    opacity: {
+      0: 0,
+      5: 0.05,
+      10: 0.1,
+      15: 0.15,
+      20: 0.2,
+      25: 0.25,
+      30: 0.3,
+      40: 0.4,
+      50: 0.5,
+      60: 0.6,
+      70: 0.7,
+      75: 0.75,
+      80: 0.8,
+      90: 0.9,
+      95: 0.95,
+      100: 1,
+    }
+  })
+
+  // Load cached resources before the app starts.
+  const isLoadingComplete = useCachedResources();
+
+  // Show nothing ( but the splashscreen ) till cached resources load.
+  if (!isLoadingComplete) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={theme}>
+      <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">Hello world</Box>
+    </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
